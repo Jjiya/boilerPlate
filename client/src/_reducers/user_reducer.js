8005f89b232da8d6168e900a6c9a788616809dec
hyeji <gpwl9610@gmail.com>
2021-03-13 20:55:23 +0900

    회원가입 기능 따라하기(code 431 error)

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,10 +1,14 @@
-import {LOGIN_USER} from '../_actions/types'
+import {LOGIN_USER, REGISTER_USER} from '../_actions/types'
 
 // reduce는 (전의 상태, 현재) 을 통해 nextState로 가게함
 export default function(state = {},action){
     switch (action.type) {
         case LOGIN_USER:
             return {...state, loginSuccess: action.payload}
+            break;
+    
+        case REGISTER_USER:
+            return {...state, register: action.payload}
             break;
     
         default:
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -14,8 +14,9 @@ function LandingPage(props) {
             display: 'flex', justifyContent: 'center', alignItems: 'center',
             width: '100%', height: '100vh'
         }}>
-            <nav>
-                <a href="/LoginPage">로그인</a>
+            <nav style={{borderRight:'1px solid black'}}>
+                <a href="/LoginPage">로그인</a><br/>
+                <a href="/RegisterPage">회원가입</a>
             </nav>
             <h2>시작페이지</h2>
         </div>
