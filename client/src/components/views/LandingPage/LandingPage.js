87332b9322b5ebc47de47edb1e9cf4acf317e8a0
hyeji <gpwl9610@gmail.com>
2021-03-13 22:18:53 +0900

    로그아웃까지 완료, but code 431에러 계속 떠서 인증체크는 보류...

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,40 @@
 import React, {useEffect} from 'react'
 import axios from 'axios';
 
+/*×
+TypeError: Cannot read property 'loginSuccess' of undefined
+
+로그인 되어있을 때만 로그아웃 하고싶은데
+어떻게 해야될지 몰라서 걍...주석...언젠가 생각나면 하겠지뭐
+
+//disabled="disabled" 이건 밑에 로그아웃 버튼에 속성넣었던 것
+
+window.onload = (response)=>{
+    if(response.payload.loginSuccess){
+        document.getElementById("logout").disabled = false;
+    }
+}
+*/
+
 function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
             .then(response => { console.log(response) })
     }, [])
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout').then(
+            response => {
+                console.log(response.data);
+                if(response.data.success){
+                    props.history.push('/login');
+                }else{
+                    alert('로그아웃 실패');
+                }
+            }
+        )
+    }
 
 
     return (
@@ -19,6 +47,7 @@ function LandingPage(props) {
                 <a href="/RegisterPage">회원가입</a>
             </nav>
             <h2>시작페이지</h2>
+            <button id ="logout" onClick={onClickHandler}>로그아웃</button>
         </div>
     )
 }
